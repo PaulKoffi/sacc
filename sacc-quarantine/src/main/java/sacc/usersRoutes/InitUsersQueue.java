e10fa7170b8a88047186bcabeebce0a95aa159b7
djotihamNABAGOU <djotiham.nabagou@etu.univ-cotedazur.fr>
2020-11-15 20:35:22 +0100

    #12 updating readme and docs files

--- sacc-quarantine/src/main/java/sacc/usersRoutes/InitUsersQueue.java
+++ sacc-quarantine/src/main/java/sacc/usersRoutes/InitUsersQueue.java
@@ -28,7 +28,7 @@ public class InitUsersQueue extends HttpServlet {
             throws ServletException, IOException {
 
 
-        for (int index =100; index < 201; index++){
+        for (int index =100; index < 1000; index++){
             String res = _gson.toJson(new User("077779"+index,"florian.ainadou"+index+"@gmail.com", false));
             Queue queue = QueueFactory.getQueue("users-queue");
             queue.add(TaskOptions.Builder.withUrl("/user").payload(res));
