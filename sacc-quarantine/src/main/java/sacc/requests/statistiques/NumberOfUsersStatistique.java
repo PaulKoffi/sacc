f4ed3a2a112903519ee8345d50f1536a9e03cde6
PaulmarieDjekinnou <paulmarie.djekinnou@outlook.fr>
2020-11-15 05:27:36 +0100

    #5 mise à jour des statistiques

--- sacc-quarantine/src/main/java/sacc/requests/statistiques/NumberOfUsersStatistique.java
+++ sacc-quarantine/src/main/java/sacc/requests/statistiques/NumberOfUsersStatistique.java
@@ -1,6 +1,14 @@
 package sacc.requests.statistiques;
 
+import com.google.api.core.ApiFuture;
 import com.google.appengine.repackaged.com.google.gson.Gson;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.DocumentSnapshot;
+import com.google.cloud.firestore.Firestore;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.FirestoreClient;
 import sacc.mocks.Statistiques;
 import sacc.models.Statistique;
 
@@ -10,19 +18,49 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Objects;
+import java.util.concurrent.ExecutionException;
 
 @WebServlet(name = "NumberOfUsersStatistique", value = "/statistiques/numberOfUsers")
 public class NumberOfUsersStatistique extends HttpServlet {
 
   private Gson _gson = new Gson();
+  private Firestore firestoreDb;
+
+  public NumberOfUsersStatistique() throws IOException {
+    GoogleCredentials credentials = GoogleCredentials.getApplicationDefault();
+    FirebaseOptions options = new FirebaseOptions.Builder()
+            .setCredentials(credentials)
+            .setProjectId("sacc-quarantine")
+            .build();
+    FirebaseApp.initializeApp(options);
+
+    firestoreDb = FirestoreClient.getFirestore();
+  }
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response)
       throws IOException {
 
-    Statistiques statistiques = new Statistiques();
+    DocumentReference docRef = firestoreDb.collection("statistics").document("sacc");
+// asynchronously retrieve the document
+    ApiFuture<DocumentSnapshot> future = docRef.get();
+// ...
+// future.get() blocks on response
+    DocumentSnapshot document = null;
+    try {
+      document = future.get();
+    } catch (InterruptedException | ExecutionException e) {
+      e.printStackTrace();
+    }
+    assert document != null;
+    if (document.exists()) {
+      System.out.println("Document data: " + document.getData());
+    } else {
+      System.out.println("No such document!");
+    }
 
-    sendAsJson(response,statistiques.getnumberOfUserStatistique());
+    sendAsJson(response, Objects.requireNonNull(document.getData()).get("numberOfUsers"));
   }
 
   private void sendAsJson( HttpServletResponse response, Object obj) throws IOException {
